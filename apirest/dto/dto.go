a67b41f7388d1ef6b386ac5aca3602b6d6691102
gopher <rossi.federico.e@gmail.com>
2019-07-25 21:09:12 -0300

    API Rest II

--- apirest/dto/dto.go
+++ apirest/dto/dto.go
@@ -2,11 +2,15 @@ package dto
 
 // DTO significa Data Transfer Object: se usa para tener pre-establecido que objeto voy a recibir o enviar
 // hacia otras aplicaciones mediante HTTP. (No es exclusivo de las API Rest, en SOAP ya se usaba)
+
+// Con `binding:"required"` decimos que el json que recibimos y que queremos mapear dentro de un MovieDTO tiene
+// que tener todos esos campos.
+// En nuestro caso si los tiene pero son string vacio, por ejemplo, es algo que validamos con el contentService
 type MovieDTO struct {
-	ID       int
-	Title    string
-	Language string
-	Budget   int64
-	Revenue  int64
-	IMDB     string
+	ID       int    `binding:"required"`
+	Title    string `binding:"required"`
+	Language string `binding:"required"`
+	Budget   int64  `binding:"required"`
+	Revenue  int64  `binding:"required"`
+	IMDB     string `binding:"required"`
 }
